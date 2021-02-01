86659bcb0cfe6c79fd4c605a6f466656d6529a1e
hynial.chen <hynials@gmail.com>
2021-02-01 20:58:18 +0800

    fun method parameter do not contain artifact classes

--- src/test/java/com/hynial/cucumber/attach/AttachMain.kt
+++ src/test/java/com/hynial/cucumber/attach/AttachMain.kt
@@ -3,18 +3,18 @@ package com.hynial.cucumber.attach
 import io.appium.java_client.AppiumDriver
 
 fun main(args: Array<String>){
-    val sid = "70659247-ae04-4fd2-9813-a940beca52e4"
+    val sid = "11cedc22-e084-4194-abfc-f33d1ccbcb82"
     val appiumDebugHelp = AppiumDebugHelp(sid)
+//    appiumDebugHelp.checkClassLoaded()
     appiumDebugHelp.artifactClass()
 
     var appiumDriver = appiumDebugHelp.attachSession()
-
-    //println(appiumDriver.pageSource)
 
     enjoy(appiumDriver)
     println()
 }
 
-fun enjoy(driver: AppiumDriver<*>){
-
+fun enjoy(driver: Any){
+    val appiumDriver = driver as AppiumDriver<*>
+    println(appiumDriver.pageSource)
 }
