fc1b7a88fcf273c98bb913d0375cf958c777645a
hynial.chen <hynials@gmail.com>
2021-02-06 00:11:14 +0800

    thread consider ?

--- src/main/java/com/hynial/factory/AppiumFactory.java
+++ src/main/java/com/hynial/factory/AppiumFactory.java
@@ -7,26 +7,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppiumFactory {
-    private static ThreadLocal<List<AppiumDriver>> appiumDriverListThreadLocal = new ThreadLocal<>(){
-        @Override
-        protected List<AppiumDriver> initialValue() {
-            return new ArrayList<>();
-        }
-    };
+    private static List<AppiumDriver> appiumDriverList = new ArrayList<>();
 
     public static AppiumDriver produce(ICreateDriver createDriver){
-        List<AppiumDriver> appiumDriverList = appiumDriverListThreadLocal.get();
         appiumDriverList.add(createDriver.create());
-        appiumDriverListThreadLocal.set(appiumDriverList);
         return getDriver();
     }
 
     public static AppiumDriver getDriver(){
-        System.out.println(Thread.currentThread().getId());
-        return appiumDriverListThreadLocal.get().get(0);
+        return appiumDriverList.get(0);
     }
 
     public static void setDriver(AppiumDriver appiumDriver){
-        appiumDriverListThreadLocal.get().add(0, appiumDriver);
+        appiumDriverList.add(0, appiumDriver);
     }
 }
