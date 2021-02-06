490d9e784b12e0e1e91e4eede1120cd4b6cd8193
hynial.chen <hynials@gmail.com>
2021-02-06 17:09:33 +0800

    add full reset field

--- src/main/java/com/hynial/wechat/entity/mobiledriver/BaseCapabilities.java
+++ src/main/java/com/hynial/wechat/entity/mobiledriver/BaseCapabilities.java
@@ -12,11 +12,13 @@ public class BaseCapabilities {
     public static String DEVICE_NAME = "deviceName";
     public static String AUTOMATION_NAME = "automationName";
     public static String NO_RESET = "noReset";
+    public static String FULL_RESET = "fullReset";
     public static String AUTO_WEBVIEW = "autoWebview";
     public static String AUTO_WEBVIEW_TIMEOUT = "autoWebviewTimeout";
     public static String ENSURE_WEBVIEWS_HAVE_PAGES = "ensureWebviewsHavePages";
     public static String RESET_KEYBOARD = "resetKeyboard";
     public static String UNICODE_KEYBOARD = "unicodeKeyboard";
+    public static String BROWSER_NAME = "browserName";
 
     @Getter(AccessLevel.PROTECTED) // public can get it
     protected String udid;
@@ -34,9 +36,12 @@ public class BaseCapabilities {
     @Getter(AccessLevel.PROTECTED)
     protected String appPath;
 
+    protected String browserName = "Browser";
+
     protected boolean clearSystemFiles;
     protected boolean autoAcceptAlerts;
     protected boolean noReset = true;
+    protected boolean fullReset = false;
     protected boolean autoWebview = false;
 
     protected boolean ensureWebviewsHavePages = false;
