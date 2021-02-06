58d3e0f8bcccef2fb42b4cea09a5f70cd869e87c
hynial.chen <hynials@gmail.com>
2021-02-06 17:10:44 +0800

    add appium service builder

--- src/test/java/com/hynial/cucumber/TestNGRunner.java
+++ src/test/java/com/hynial/cucumber/TestNGRunner.java
@@ -1,16 +1,23 @@
 package com.hynial.cucumber;
 
 import com.hynial.cucumber.attach.AppiumDebugHelp;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @CucumberOptions(
         dryRun = false,
@@ -63,11 +70,60 @@ public class TestNGRunner extends AbstractTestNGCucumberTests {
         memberValues.forEach((x,y) -> {
             options.put(x, y);
         });
+
+
     }
 
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
+    }
+
+    private AppiumDriverLocalService appiumService;
+
+    public void startAppiumServer() throws IOException {
+
+        int port = 4723;
+        String nodeJS_Path = "C:/Program Files/NodeJS/node.exe";
+        String appiumJS_Path = "C:/Program Files/Appium/node_modules/appium/bin/appium.js";
+
+        String osName = System.getProperty("os.name");
+
+        if (osName.contains("Mac")) {
+            appiumService = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                    .usingDriverExecutable(new File(("/usr/local/bin/node")))
+                    .withAppiumJS(new File(("/usr/local/bin/appium")))
+                    .withIPAddress("0.0.0.0")
+                    .usingPort(port)
+                    .withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+                    .withLogFile(new File("build/appium.log")));
+        } else if (osName.contains("Windows")) {
+            appiumService = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                    .usingDriverExecutable(new File(nodeJS_Path))
+                    .withAppiumJS(new File(appiumJS_Path))
+                    .withIPAddress("0.0.0.0")
+                    .usingPort(port)
+                    .withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+                    .withLogFile(new File("build/appium.log")));
+        }
+        appiumService.start();
+
+    }
+
+    public void tearDown() {
+        try {
+            appiumService.stop();
+        } catch (Exception e) {
+            System.out.println("Exception while running Tear down :" + e.getMessage());
+        }
+    }
+
+    public static void changeDriverContextToWeb(AppiumDriver driver) {
+        Set<String> allContext = driver.getContextHandles();
+        for (String context : allContext) {
+            if (context.contains("WEBVIEW"))
+                driver.context(context);
+        }
     }
 }
